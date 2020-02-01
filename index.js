d229d6c7497b9086487f4b5fdd4e075062142669
foxthefox <github@wysiwis.net>
2020-02-01 13:43:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = function(homebridge) {
   
   // For platform plugin to be considered as dynamic platform plugin,
   // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
-  homebridge.registerPlatform("homebridge-cul", "CulPlatform", CulPlatform, true);
+  homebridge.registerPlatform("homebridge-cul", "CUL", CulPlatform, true);
 }
 
 // Platform constructor
